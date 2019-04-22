a9725f4e2da5a2556dbd66709195c9c1bfd005c5
mgiota <giota85@gmail.com>
2019-04-22 19:10:44 +0200

    3.6 Redux

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
+import { connect } from 'react-redux';
 
-export class MovieView extends React.Component {
-
-  constructor() {
-    super();
-
-    this.state = {};
-  }
-
-  render() {
-    const { movie } = this.props;
-    if (!movie) return null;
-    return (
-       <div className="movie-view">
-         <div className="movie-title">{movie.Title}</div>
-         <div className="movie-description">{movie.Description}</div>
-         <div className="movie-genre">{movie.Genre.Name}</div>
-         <div className="movie-director">{movie.Director.Name}</div>
-         <Link to={`/directors/${movie.Director.Name}`}>
-          <Button variant="link">Director</Button>
-        </Link>
-       </div>
-    );
-  }
+function MovieView(props) {
+  const { movies, movieId } = props;
+  if (!movies || !movies.length) return null;
+  const movie = movies.find(m => m._id == movieId);
+  return (
+     <div className="movie-view">
+       <div className="movie-title">{movie.Title}</div>
+       <div className="movie-description">{movie.Description}</div>
+       <div className="movie-genre">{movie.Genre.Name}</div>
+       <div className="movie-director">{movie.Director.Name}</div>
+       <Link to={`/directors/${movie.Director.Name}`}>
+        <Button variant="link">Director</Button>
+      </Link>
+     </div>
+  );
 }
+
+export default connect(({movies}) => ({movies}))(MovieView);
