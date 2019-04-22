da85a23d056cb5d786e6a4ed045a0820b5a75fd2
mgiota <giota85@gmail.com>
2019-04-22 20:57:16 +0200

    3.6: visibility filter

--- client-2/src/components/movies-list/movies-list.jsx
+++ client-2/src/components/movies-list/movies-list.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
 import { MovieCard } from '../movie-card/movie-card';
 
 const mapStateToProps = state => {
   console.log(state, '!!state')
   const { movies, visibilityFilter, sortColumn } = state;
 
-  let sortedMovies = movies.concat().sort((a, b) => {
+  let moviesToShow = movies.concat().sort((a, b) => {
     if (a[sortColumn] < b[sortColumn]) return -1;
     if (a[sortColumn] > b[sortColumn]) return 1;
     return 0;
   });
 
+  if (visibilityFilter !== '') {
+    moviesToShow = moviesToShow.filter(m => m.Title.toLowerCase().includes(visibilityFilter));
+  }
 
-  return { movies: sortedMovies };
+  return { movies: moviesToShow };
 };
 
 function MoviesList(props) {
@@ -22,7 +26,11 @@ function MoviesList(props) {
 
   if (!movies) return <div className="main-view"/>;
 
-  return movies.map(m => <MovieCard key={m.id} movie={m}/>);
+  return <div className="movies-list">
+    <VisibilityFilterInput/>
+    {movies.map(m => <MovieCard key={m._id} movie={m}/>)}
+  </div>;
+
 }
 
 export default connect(mapStateToProps)(MoviesList);
